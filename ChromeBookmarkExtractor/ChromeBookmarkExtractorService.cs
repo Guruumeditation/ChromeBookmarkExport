a54bf803a1287a9df3296d4a183d58c47522a3fe
olivier matis <olivier_matis@hotmail.com>
2015-07-31 18:42:32 +0200

    First version

---
+++ ChromeBookmarkExtractor/ChromeBookmarkExtractorService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace ChromeBookmarkExtractor
+{
+    public static class ChromeBookmarkExtractorService
+    {
+        public static string Extract(string filepath, string destinationpath)
+        {
+            var errors = 0;
+            var processed = 0;
+
+            var rawtext = File.ReadAllText(filepath);
+
+            var o = JObject.Parse(rawtext);
+
+            var barchildren = o["roots"]["bookmark_bar"]["children"];
+
+            var otherchildren = o["roots"]["other"]["children"];
+
+            var totalchildren = barchildren.Union(otherchildren);
+
+            var tempdestinationpath = destinationpath;
+
+            foreach (var child in totalchildren)
+            {
+                try
+                {
+                    var safename = GetSafeFilename(child["name"].Value<string>());
+
+                    if (child["type"].Value<string>() == "folder")
+                    {
+                        Directory.CreateDirectory(safename);
+
+                        tempdestinationpath = destinationpath + @"\" + safename + @"\";
+
+                        continue;
+                    }
+                    var file = File.OpenWrite(tempdestinationpath + @"\" + safename + ".url");
+                    var text = $"[InternetShortcut]{Environment.NewLine}URL={child["url"].Value<string>()}";
+                    var bytearray = Encoding.UTF8.GetBytes(text);
+                    file.Write(bytearray, 0, bytearray.Length);
+                    file.Close();
+
+                    processed++;
+                }
+                catch (Exception e)
+                {
+                    errors++;
+                    Console.WriteLine(e);
+                }
+            }
+
+            return $"Processed : {processed}, errors : {errors}";
+        }
+
+        private static string GetSafeFilename(string filename)
+        {
+
+            return string.Join("_", filename.Split(Path.GetInvalidFileNameChars()));
+
+        }
+    }
+}
---
+++ ChromeBookmarkExtractor/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Forms;
+using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
+
+namespace ChromeBookmarkExtractor
+{
+    public partial class MainWindow
+    {
+        private readonly string _initialDirectory;
+
+        private readonly string _initialDestinationDirectory;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var p = System.Threading.Thread.CurrentPrincipal as System.Security.Principal.WindowsPrincipal;
+
+            _initialDirectory = $"C:\\Users\\{p?.Identity.Name}\\AppData\\Local\\Google\\Chrome\\User Data\\Default";
+
+            _initialDestinationDirectory = System.AppDomain.CurrentDomain.BaseDirectory;
+
+            if (!Directory.Exists(_initialDirectory))
+                _initialDirectory = Path.GetDirectoryName(_initialDestinationDirectory);
+
+            TB_FilePath.Text = _initialDirectory + @"\Bookmarks";
+
+            TB_DestinationPath.Text = _initialDirectory;
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            
+            var file = new OpenFileDialog { FileName = "Bookmarks", InitialDirectory = _initialDirectory };
+            if (file.ShowDialog() == true)
+            {
+                TB_FilePath.Text = file.FileName;
+
+                BT_Extract.IsEnabled = !string.IsNullOrEmpty(TB_FilePath.Text);
+            }
+        }
+
+
+        private void DestinationSelection_OnClick(object sender, RoutedEventArgs e)
+        {
+            var file = new FolderBrowserDialog() {SelectedPath = _initialDirectory};
+
+            if (file.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                TB_DestinationPath.Text = file.SelectedPath;
+            }
+        }
+
+        private void Extract_OnClick(object sender, RoutedEventArgs e)
+        {
+            Info.Text = ChromeBookmarkExtractorService.Extract(TB_FilePath.Text, TB_DestinationPath.Text);
+        }
+    }
+}
